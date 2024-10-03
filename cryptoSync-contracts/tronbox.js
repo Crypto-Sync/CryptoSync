7f2785ca5e68074b6fa59d01d233b325a94cef7c
Jay-Sojitra <jaysojitra1011@gmail.com>
2024-10-03 11:33:10 +0530

    enable optomizer

--- cryptoSync-contracts/tronbox.js
+++ cryptoSync-contracts/tronbox.js
@@ -56,10 +56,10 @@ module.exports = {
   },
   // solc compiler optimize
   solc: {
-  //   optimizer: {
-  //     enabled: true,
-  //     runs: 200
-  //   },
-  //   evmVersion: 'istanbul'
+    optimizer: {
+      enabled: true,
+      runs: 200
+    },
+    evmVersion: 'istanbul'
   }
 }
