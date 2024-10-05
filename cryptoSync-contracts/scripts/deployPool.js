cc6a4ea38ee985b2e1f55b0e58e1ca9979dcff8b
Jay-Sojitra <jaysojitra1011@gmail.com>
2024-10-05 16:22:37 +0530

    change script

--- cryptoSync-contracts/scripts/deployPool.js
+++ cryptoSync-contracts/scripts/deployPool.js
@@ -7,35 +7,39 @@ dotenv.config();
 const contractJson = JSON.parse(fs.readFileSync('./build/contracts/PoolFactory.json', 'utf8'));
 const abi = contractJson.abi;
 
-const poolFactoryAddress = "TAdRkJmETfaQDwkq1VADnz1qU7JW9Ej7nf"; 
+const poolFactoryAddress = "TAdRkJmETfaQDwkq1VADnz1qU7JW9Ej7nf";
 
 const privateKey = process.env.PRIVATE_KEY;
+// const tronWeb = new TronWeb({
+//   fullHost: 'https://nile.trongrid.io',
+//   headers: { "TRON-PRO-API-KEY": process.env.TRONGRID_API_KEY },
+//   privateKey
+// });
 const tronWeb = new TronWeb({
   fullHost: 'https://nile.trongrid.io',
-  headers: { "TRON-PRO-API-KEY": process.env.TRONGRID_API_KEY },
   privateKey
 });
-console.log('tronWeb',tronWeb);
+console.log('tronWeb', tronWeb);
 
 const params = [
-    ['TWYiT6zVWEH8gkp14YSPTyTjt8MXNbvVud', 'TUQJvMCiPfaYLDyQg8cKkK64JSkUVZh4qq'],
-    ['100000000000000000000', '2000000'],
-    ['5000', '5000'],
-    '1000',
-    ['15000', '15000'],
-    ['1000000', '1000000'],
-    '3600'
+  ['TWYiT6zVWEH8gkp14YSPTyTjt8MXNbvVud', 'TUQJvMCiPfaYLDyQg8cKkK64JSkUVZh4qq'],
+  ['100000000000000000000', '100000000000000000000'],
+  ['5000', '5000'],
+  '1000',
+  ['10500', '10500'],
+  ['1000000', '50000'],
+  '86400'
 ];
 
 // Example approval script (run this before creating pool)
 const approveTokens = async () => {
-    for (const tokenAddress of params[0]) {
-        const tokenContract = await tronWeb.contract().at(tokenAddress);
-        await tokenContract.approve(
-            poolFactoryAddress, 
-            '115792089237316195423570985008687907853269984665640564039457584007913129639935' // Max approval
-        ).send();
-    }
+  for (const tokenAddress of params[0]) {
+    const tokenContract = await tronWeb.contract().at(tokenAddress);
+    await tokenContract.approve(
+      poolFactoryAddress,
+      '115792089237316195423570985008687907853269984665640564039457584007913129639935' // Max approval
+    ).send();
+  }
 };
 
 const createPool = async () => {
@@ -47,12 +51,12 @@ const createPool = async () => {
     console.log('Contract instantiated at address:', poolFactoryAddress);
 
     // approveTokens();
-    
+
 
     const tx = await poolFactoryContract.createPool(params).send({
-        feeLimit: 1000 * 1e6,
-        callValue: 0
-      });
+      feeLimit: 1000 * 1e6,
+      callValue: 0
+    });
 
     console.log('Pool created successfully, transaction ID:', tx);
     process.exit(0);
