c5313e2a8552f6a968784045132ae815055cb324
jaydip676 <jaydip.lamprostech@gmail.com>
2024-10-15 18:48:31 +0530

    bug fixes

--- cryptosync-frontend/app/pool/[id]/page.tsx
+++ cryptosync-frontend/app/pool/[id]/page.tsx
@@ -1,14 +1,10 @@
-import { CryptoPrices, fetchCryptoPrices } from "@/lib/fetchCryptoPrices";
 import dynamic from "next/dynamic";
 
 const SinglePool = dynamic(() => import("@/components/SinglePool"), { ssr: false })
 export default async function page() {
-
-
-    const prices: CryptoPrices = await fetchCryptoPrices();
     return (
         <div className="py-20 min-h-screen flex items-center justify-center">
-            <SinglePool prices={prices} />
+            <SinglePool />
         </div>
     );
 }
